1d1101864f2197a227dd9b56c401f4f9f4e4be05
Jakob Skjerning <jakob@mentalized.net>
2019-04-01 16:20:21 +0200

    Deploy from our fork of Sharetribe

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "tumlino"
+set :repo_url, "git@github.com:substancelab/sharetribe.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
