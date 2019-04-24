8ae7f55d19f27809e3462e5f79c7836d64fd74ef
Jakob Skjerning <jakob@mentalized.net>
2019-04-24 12:02:57 +0200

    Keep search indexes reasonably up to date

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,3 +8,8 @@ every 1.day do
   # Clean up expired auth tokens
   rake "sharetribe:delete_expired_auth_tokens"
 end
+
+every 1.hour do
+  # Keep search indexes reasonably up to date
+  rake "ts:index"
+end
