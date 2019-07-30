d98ec9398ff32452a9eb92ee3dcef060d03d8063
Jakob Skjerning <jakob@mentalized.net>
2019-07-30 13:32:28 +0200

    Use rbenv for our job types

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,9 @@
+if @rbenv_root
+  job_type :rake,    %{cd :path && :environment_variable=:environment :rbenv_root/bin/rbenv exec bundle exec rake :task --silent :output}
+  job_type :runner,  %{cd :path && :rbenv_root/bin/rbenv exec bundle exec rails runner -e :environment ':task' :output}
+  job_type :script,  %{cd :path && :environment_variable=:environment :rbenv_root/bin/rbenv exec bundle exec script/:task :output}
+end
+
 every 1.day do
   # Clean up expired sessions
   runner "ActiveSessionsHelper.cleanup"
